31f6a219b05c23891b987e93ce6f31daf0988bab
Carlos Astorga <databook.one@gmail.com>
2024-08-21 17:55:30 +0200

    ex00

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+std::string convertToUp(const std::string& message)
+{
+    std::string result = message;
+    for (char& c : result)
+	{
+        c = std::toupper(c);
+    }
+    return (result);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc > 1) {
+        std::string fullMessage;
+        for (int i = 1; i < argc; ++i)
+		{
+            std::string message = argv[i];
+            std::string result = convertToUp(message);
+            fullMessage += result;
+            if (i < argc - 1)
+			{
+                fullMessage += " ";
+            }
+        }
+        std::cout << fullMessage << std::endl;
+    }
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return (0);
+}
