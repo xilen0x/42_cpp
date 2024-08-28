3210699e21e90bf6b00e60370079c7441af6755a
Carlos Astorga <databook.one@gmail.com>
2024-08-28 18:48:15 +0200

    wip 0001

---
+++ module00/ex01/PhoneBook.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: castorga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 17:45:01 by castorga          #+#    #+#             */
+/*   Updated: 2024/08/28 17:45:04 by castorga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+/*
+It has an array of contacts.
+◦ It can store a maximum of 8 contacts. If the user tries to add a 9th contact,
+replace the oldest one by the new one.
+◦ Please note that dynamic allocation is forbidden.
+*/
+class PhoneBook
+{
+	private:
+		
+	public:
+		
+};
+
+
+
+#endif
