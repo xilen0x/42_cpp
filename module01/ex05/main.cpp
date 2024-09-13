6d8c551ff51c5f70b1e5674807cda7952613cd9d
Carlos Astorga <carlos.astorgax@gmail.com>
2024-09-13 16:46:44 +0200

    0105 ended

---
+++ module01/ex05/main.cpp
@@ -0,0 +1,50 @@
+# include "Harl.hpp"
+#include <cstdlib>  // Para rand() y srand()
+#include <ctime>    // Para time()
+
+Harl::Harl(){}
+
+void Harl::debug()
+{	std::cout << "DEBUG:" << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;}
+
+void Harl::info()
+{	std::cout << "INFO:" << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;}
+
+void Harl::warning()
+{	std::cout << "WARNING:" << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;}
+
+void Harl::error()
+{	std::cout << "ERROR:" << "This is unacceptable! I want to speak to the manager now." << std::endl;}
+
+void Harl::complain(std::string level)
+{
+    void (Harl::*functions[])(void) = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+    std::string levels[] = { "debug", "info", "warning", "error" };
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (levels[i] == level)
+        {
+            (this->*functions[i])();
+            return;
+        }
+    }
+    std::cout << "Invalid level" << std::endl;
+}
+
+// #include <cstdio>
+int main()
+{
+	Harl	instance;
+
+	instance.complain("debug");
+	std::cout << std::endl;
+	instance.complain("info");
+	std::cout << std::endl;
+	instance.complain("warning");
+	std::cout << std::endl;
+	instance.complain("error");
+	return (0);
+}
+
+Harl::~Harl(){}
