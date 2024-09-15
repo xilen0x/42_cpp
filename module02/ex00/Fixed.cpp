ca662996b4fe2b23ad17e08d9ec943e6ad6466f8
Carlos Astorga <carlos.astorgax@gmail.com>
2024-09-15 18:36:50 +0200

    up

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -1,37 +1,42 @@
 #include "Fixed.hpp"
 
 // Default constructor
-Fixed::Fixed(){
-	_fixedPointValue = 0;
+Fixed::Fixed()
+{
+	this->_fixedPointValue = 0;
 	std::cout << "Default constructor called" << std::endl;
 }
 
 // Copy constructor
-Fixed::Fixed( const Fixed &copyConstructor ){
+Fixed::Fixed( const Fixed &copyConstructor )
+{
 	std::cout << "Copy constructor called" << std::endl;
 	*this = copyConstructor;
 }
 
 // Copy assignment operator overload
-Fixed &Fixed::operator=( const Fixed &copyConstructor ){
+Fixed &Fixed::operator=( const Fixed &copyConstructor )
+{
 	std::cout << "Copy assignment operator called" << std::endl;
-	_fixedPointValue = copyConstructor.getRawBits();
+	this->_fixedPointValue = copyConstructor.getRawBits();
 	return (*this);
+}
+
+// Get the raw bits
+int Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixedPointValue);
+}
+
+// Set the raw bits
+void Fixed::setRawBits( int const raw )
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_fixedPointValue = raw;
 }
 
 // Destructor
 Fixed::~Fixed(){
 	std::cout << "Destructor called" << std::endl;
-}
-
-// Get the raw bits
-int Fixed::getRawBits( void ) const{
-	std::cout << "getRawBits member function called" << std::endl;
-	return (_fixedPointValue);
-}
-
-// Set the raw bits
-void Fixed::setRawBits( int const raw ){
-	std::cout << "setRawBits member function called" << std::endl;
-	_fixedPointValue = raw;
 }
